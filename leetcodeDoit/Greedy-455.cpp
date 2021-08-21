2d6bcab85b4fd6ee1b27a204229224dd207c6c6b
佘鑫辉 <shexinhui@talkweb.com.cn>
2021-08-21 22:57:40 +0800

    添加项目文件。

---
+++ leetcodeDoit/Greedy-455.cpp
@@ -0,0 +1,25 @@
+﻿
+#include<vector>
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+/*
+*输入: g = [1,2,3], s = [1,1]
+*输出: 1
+* 题目:对每个孩子 i，都有一个胃口值 g[i]，这是能让孩子们满足胃口的饼干的最小尺寸；
+*并且每块饼干 j，都有一个尺寸 s[j] 。如果 s[j] >= g[i]，我们可以将这个饼干 j 分配给孩子 i ，
+*这个孩子会得到满足。你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
+*/
+int findContentChildren(vector<int>& g, vector<int>& s) {
+    sort(g);
+
+}
+
+int main()
+{
+    vector<int>g = { 1,2,3 };
+    vector<int>s = { 1,1 };
+
+
+}
